34aaf39279c21cbfba9142d2280f6c2a4c4d9bc1
kk08112544 <89054512+kk08112544@users.noreply.github.com>
2022-10-07 03:44:35 +0700

    Add files via upload

---
+++ src/pages/ManPayPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-page>
+    <div class="h-[15vh] bg-[#f9b805e0] flex justify-center items-center w-full gap-5">
+      <span class="text-[20px] text-red">Manage Payment</span>
+    </div>
+    <div class="h-[100vh] bg-[#B18BEF] flex justify-center   w-full gap-5">
+       <div class="flex-col flex mt-6">
+         <div class="flex-row">
+          <q-table
+            title="Payment Manage"
+            :rows="rows"
+            :columns="columns"
+            row-key="name"
+          />
+         </div>
+         <br>
+         <div class="flex-col justify-center items-center">
+            <q-btn color="red" no-caps  label="Add Payment"  clickable
+            v-ripple
+            to="/AddPay" />
+         </div>
+       </div>
+    </div>
+  </q-page>
+</template>
+<script>
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Payment Project',
+    align: 'left',
+    field: row => row.name,
+  },
+  { name: 'calories', align: 'center', label: 'Payment Details', field: 'calories', sortable: true },
+]
+
+const rows = [
+  {
+    name: 'Truemoney',
+    calories: '061-345-3424',
+    carbs: "Normal",
+
+  }
+
+]
+
+export default {
+  setup () {
+    return {
+      columns,
+      rows
+    }
+  }
+}
+</script>
--- src/pages/PayPage.vue
+++ src/pages/PayPage.vue
@@ -1,124 +1,126 @@
 <template>
-  <q-page>
-    <!-- <div class="flex items-center flex-col gap-20 bg-[#B18BEF] h-[100vh]">
-      <div
-        class="bg-white/50 rounded-[65px] flex justify-center items-center bg-shadow-custom flex-col w-full px-2 py-10"
-      >
-        <span class="text-[15px]">Payment Method</span>
-        <div class="flex-row flex"></div>
-        <div></div>
-        <div></div>
-        <div class="flex-row flex">
-          <div class="flex-col flex"></div>
-          <div class="flex-col flex"></div>
-        </div>
-        <div></div>
-      </div>
-    </div> -->
+   <q-page
+    class="flex justify-center items-center flex-col gap-10 bg-[#B18BEF] h-[100vh]"
+  >
     <div
-      class="flex justify-center items-center flex-col gap-10 bg-[#B18BEF] h-[100vh]"
+      class="bg-white/50 rounded-[65px] flex justify-center items-start flex-col w-full px-2 py-10 bg-shadow-custom space-y-4"
     >
-      <!-- <div
-        class="bg-white/50 rounded-[65px] flex justify-center items-center bg-shadow-custom flex-col w-full px-2 py-10"
-      >
-        <span class="text-[15px]">Payment Method</span>
-        <div class="flex-row flex gap-4">
-          <div class="flex-col flex">
-            <q-img src="pay/credit.png" class="w-[48px] h-[37px]"></q-img>
-          </div>
-          <div class="flex-col flex">
-            <q-img src="pay/paypal.png" class="w-[139px] h-[78px]"></q-img>
-          </div>
-          <div class="flex-col flex">
-            <q-img src="pay/promptpay.png" class="w-[170px] h-[62px]"></q-img>
-          </div>
-          <div class="flex-col flex">
-            <q-img src="pay/truemoney.png" class="w-[140px] h-[70px]"></q-img>
-          </div>
-        </div>
-        <span class="text-[12px]">Payment Details</span>
-        <div>
-          <q-input></q-input>
-        </div>
-        <div></div>
-        <div class="flex-row flex">
-          <div class="flex-col flex"></div>
-          <div class="flex-col flex"></div>
-        </div>
-        <div></div>
-      </div> -->
-      <div
-        class="bg-white/50 rounded-[20px] gap-10 flex items-center justify-center w-full rounded-[30px] px-14 py-8"
-      >
-        <div class="q-pt-none">
-          <span class="text-[10px] sm:text-[20px]">Select the Payment.</span>
-        </div>
 
-        <div
-          class="amount-border p-5 flex flex-col activeCard items-center"
-          v-for="(item, i) in Pay"
-          :key="i"
+       <div class=""><span class="text-[25px] ml-10">Payment Methods</span></div>
+       <div class="flex-row flex gap-7 ml-20 space-x-4">
+          <div class="flex-col items-start">
+             <q-img src="pay/credit.png" class="w-[48px] h-[37px]"></q-img>
+          </div>
+          <!-- <div class="flex-col items-start">
+             <q-img src="pay/promptpay.png" class="w-[174px] h-[58px]"></q-img>
+          </div>
+          <div class="flex-col items-start">
+             <q-img src="pay/truemoney.png" class="w-[82px] h-[62px]"></q-img>
+          </div>
+          <div class="flex-col items-start">
+             <q-img src="pay/paypal.png" class="w-[112px] h-[55px]"></q-img>
+          </div> -->
+       </div>
+       <div class=""><span class="text-[20px] ml-10">Payment Details</span></div>
+       <!-- Credit Card -->
+       <div class="flex-row flex ml-10">
+        <q-input
+        rounded outlined
+            v-model="name"
+            label=" Enter name on card"
+            bg-color="white"
+            class="w-[300px] t-[250px]"
         >
-          <div>
-            <img class="max-h-10" :src="item.png" />
+        <template v-slot:prepend>
+          <q-img src="icons/visa.png" class="h-[36px] w-[74px]"/>
 
-            <div>
-              <span class="text-bold"
-                >{{ item.gold }} <br />{{ item.baht }}</span
-              >
-            </div>
-          </div>
+        </template>
+
+        </q-input>
+       </div>
+       <div class="flex-row flex ml-10">
+        <q-input
+        rounded outlined
+            v-model="cardnum"
+            label="Card Number"
+            bg-color="white"
+            class="w-[310px] t-[300px]"
+        >
+        </q-input>
+       </div>
+       <div class="flex-row flex ml-10 gap-9">
+        <q-input
+        rounded outlined
+            v-model="experate"
+            label="Experation"
+            bg-color="white"
+            class="w-[250px] t-[200px]"
+        >
+        </q-input>
+        <q-input
+        rounded outlined
+            v-model="cvv"
+            label="CVV Code"
+            bg-color="white"
+            class="w-[300px] t-[250px]"
+        >
+        </q-input>
+       </div>
+       <!-- TrueMoney -->
+       <!-- <div class="flex-row flex ml-10 gap-5">
+         <div class="flex-col flex">
+          <q-input  class="w-[250px] t-[50px]"  rounded outlined v-model="text" label="Enter mobile number" bg-color="white" />
+
         </div>
-      </div>
+         <br>
+         <div class="flex-col flex">
+          <q-btn color="red" text-color="white" no-caps label="sent" class="w-[70px] h-[30px]" />
+        </div>
+       </div>
+       <div class="flex-col flex ml-10 ">
+          <q-input  class="w-[200px] h-[50px]"  rounded outlined v-model="text" label="OTP" bg-color="white" />
+       </div> -->
+       <!-- PayPal -->
+       <!-- <div class="flex row-flex ml-10 gap-5">
+          <q-input class="w-[320px] h-[50px]" rounded outlined bg-color="white" v-model="text" type="text" label="Enter your email or ID user" />
+       </div>
+       <div class="flex row-flex ml-10 gap-5">
+          <q-input class="w-[270px] h-[50px]" rounded outlined bg-color="white" v-model="text" type="text" label="name" />
+          <q-input class="w-[300px] h-[50px]" rounded outlined bg-color="white" v-model="text" type="text" label="Lastname" />
+       </div> -->
+       <!-- promptpay -->
+       <div class="flex row-flex ml-10">
+        <!-- <div class="flex-col flex gap-5 ">
+           <q-img src="pay/header.png" class="w-[150px] ml-14" />
+           <q-img src="pay/qrcode.png" class="w-[200px] h-[150px] ml-7" />
+        </div> -->
+        <!-- <div class="flex-col flex space-y-5  mb-10">
+            <div class="flex row-flex">
+               <q-input class="w-[270px] h-[50px]" v-model="text " rounded outlined bg-color="white" type="text" label="payment date" />
+            </div>
+            <div class="flex row-flex">
+               <q-input class="w-[270px] h-[50px]" v-model="text" rounded outlined bg-color="white" type="text" label="time of payment" />
+            </div>
+            <div class="flex row-flex">
+               <q-input class="w-[270px] h-[50px]" v-model="text" rounded outlined bg-color="white" type="text" label="amount" />
+            </div>
+            <div class="flex row-flex">
+
+            </div>
+        </div> -->
+
+        </div>
+
+       <span class="text-[10px] ml-10">BY clicking "Confirm Payment" I agree to the components term of services</span>
+       <div class="ml-10">
+            <q-btn color="red" no-caps  label="Confirm"  clickable
+            v-ripple
+            to="/Status" />
+       </div>
     </div>
   </q-page>
 </template>
 <script>
-import { defineComponent, ref } from "vue";
-export default defineComponent({
-  name: "TopUpDetailsPage",
-  setup() {
-    const Pay = [
-      {
-        png: "pay/credit.png",
-      },
-      {
-        png: "pay/paypal.png",
-      },
-      {
-        png: "pay/promptpay.png",
-      },
-      {
-        png: "pay/truemoney.png",
-      },
-    ];
-    return { Pay };
-  },
-});
+
+
 </script>
-<style scoped>
-.activeCard {
-  width: 250px;
-  transition: 0.2s ease-out;
-}
-.activeCard:hover {
-  width: 300px;
-}
-.bg {
-  background: radial-gradient(
-    249.11% 67.08% at 52.55% 46.23%,
-    #6214e2 0%,
-    #e2d4f9 0.01%,
-    #e2d4f9 63.02%,
-    #ebe1fb 73.96%,
-    #f7f3fd 89.58%,
-    #ffffff 100%
-  );
-}
-.amount-border {
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25),
-    inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 20px;
-}
-</style>
-Footer © 2022 GitHub, Inc. Footer navigation Terms Privacy
